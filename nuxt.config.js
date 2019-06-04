b3c3d3bdf6167d4923da3be21fbcacffc7f2ac64
Michiel Koning <mail@michielkoning.nl>
2019-06-04 19:55:20 +0200

    fix generateor

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,23 +98,5 @@ export default {
         }
       }
     }
-  },
-  generate: {
-    async routes() {
-      const response = await axios.get(`${baseUrl}wp/v2/posts/?per_page=100`)
-      const posts = response.data.map(post => post.slug)
-      const urls = ['biography', ...posts]
-
-      return urls
-    }
-  },
-  sitemap: {
-    hostname: 'https://nordgarden.michielkoning.nl/',
-    async routes() {
-      const response = await axios.get(`${baseUrl}wp/v2/posts/?per_page=100`)
-      return response.data.map(
-        post => `https://nordgarden.michielkoning.nl/${post.slug}`
-      )
-    }
   }
 }
