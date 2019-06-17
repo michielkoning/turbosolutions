b3b4714b061046ae3c235a28af1824a2e635abec
Michiel Koning <mail@michielkoning.nl>
2019-06-17 15:13:15 +0200

    notchwrapper

--- components/blocks/BlockMap.vue
+++ components/blocks/BlockMap.vue
@@ -1,11 +1,18 @@
 <template>
-  <GmapMap :center="position" :zoom="12" class="map">
-    <GmapMarker :position="position"></GmapMarker>
-  </GmapMap>
+  <notch-wrapper>
+    <GmapMap :center="position" :zoom="12" class="map">
+      <GmapMarker :position="position"></GmapMarker>
+    </GmapMap>
+  </notch-wrapper>
 </template>
 
 <script>
+import NotchWrapper from '@/components/NotchWrapper.vue'
+
 export default {
+  components: {
+    NotchWrapper,
+  },
   data() {
     return {
       position: {
