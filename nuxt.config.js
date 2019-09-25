31fe45eca7b695bd2a84447031df05fcc7031176
Michiel Koning <michiel.koning@valtech.com>
2019-09-25 11:42:50 +0200

    do not track

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,15 @@ export default {
   },
   googleAnalytics: {
     id: process.env.NUXT_ENV_GOOGLE_ANALYTICS_KEY,
-    set: [{ field: 'anonymizeIp', value: true }]
+    set: [{ field: 'anonymizeIp', value: true }],
+    disabled: () => {
+      const doNotTrack =
+        navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack
+      if (doNotTrack !== '1' && doNotTrack !== 'yes') {
+        return false
+      }
+      return true
+    }
   },
   /*
    ** Build configuration
