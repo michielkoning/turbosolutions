f6ec10edae2ecc541602fd48be00b0f32ecaeb6f
Michiel Koning <mail@michielkoning.nl>
2019-10-13 11:30:53 +0200

    new text what do we do

--- components/blocks/BlockWhatWeDo.vue
+++ components/blocks/BlockWhatWeDo.vue
@@ -2,17 +2,20 @@
   <block-wrapper>
     <skip-link-title class="title" :title="$t('pages.whatWeDo.title')" />
     <h2>{{ $t('pages.whatWeDo.intro') }}</h2>
-    <p v-for="text in $t('pages.whatWeDo.text')" :key="text">
+    <p v-for="text in $t('pages.whatWeDo.top.text')" :key="text">
       {{ text }}
     </p>
     <ul>
-      <li v-for="text in $t('pages.whatWeDo.list')" :key="text">
+      <li v-for="text in $t('pages.whatWeDo.top.list')" :key="text">
         {{ text }}
       </li>
     </ul>
-    <div class="image-wrapper">
-      <app-image src="/media/images/tse_mechanics.jpg?nf_resize=fit&w=770&h=425" width="770" height="425" />
+    <div :class="$style['image-wrapper']">
+      <app-image :class="$style['image']" src="/media/images/tse_mechanics.jpg?nf_resize=fit&w=770&h=425" width="770" height="425" />
     </div>
+    <p v-for="text in $t('pages.whatWeDo.bottom')" :key="text">
+      {{ text }}
+    </p>
   </block-wrapper>
 </template>
 
@@ -30,8 +33,8 @@ export default {
 }
 </script>
 
-<style lang="postcss" scoped>
-img {
+<style lang="postcss" module>
+.image {
   display: block;
   width: 100%;
 }
